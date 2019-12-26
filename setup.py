bd71ef2c330114823cbe05bfc75e6ff35cf77384
Vinay Pai <vinay@vinaypai.com>
2019-12-26 13:54:16 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='django-simple-cms',
-    version='0.1.2',
+    version='0.1.3',
     author='Vinay Pai',
     author_email='vinay@vinaypai.com',
     description='A simple  CMS for django',
